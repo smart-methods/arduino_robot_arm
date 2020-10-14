ca1c095d69929b8ce0ff8f0a9bf8e2640b7b6829
Smart Methods <72750805+smart-methods@users.noreply.github.com>
2020-10-14 15:57:44 +0300

    Add files via upload

---
+++ robot_arm_pkg/scripts/joint_states_to_gazebo.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Header
+from trajectory_msgs.msg import JointTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.position)
+    pub = rospy.Publisher('arm_controller/command', JointTrajectory, queue_size=10)
+
+    joints_str = JointTrajectory()
+    joints_str.header = Header()
+    joints_str.header.stamp = rospy.Time.now()
+    joints_str.joint_names = ['base_joint', 'shoulder', 'elbow', 'wrist']
+    point=JointTrajectoryPoint()
+    point.positions = [data.position[0], data.position[1], data.position[2], data.position[3]]
+    point.time_from_start = rospy.Duration(2)
+    joints_str.points.append(point)
+
+    pub.publish(joints_str)
+    rospy.loginfo("position updated")
+
+
+
+def listener():
+    rospy.init_node('states', anonymous=True)
+    rospy.Subscriber("joint_states", JointState, callback)
+
+    rospy.spin()
+
+
+def talker():
+    pub = rospy.Publisher('arm_controller/command', JointTrajectory, queue_size=10)
+    rospy.init_node('trajectory')
+    rate = rospy.Rate(10) # 10hz
+    joints_str = JointTrajectory()
+    joints_str.header = Header()
+    joints_str.header.stamp = rospy.Time.now()
+    joints_str.joint_names = ['base_joint', 'shoulder', 'elbow', 'wrist']
+    point=JointTrajectoryPoint()
+    point.positions = [0.5, 0.5, 0.5, 0.5]
+    point.time_from_start = rospy.Duration(2)
+    joints_str.points.append(point)
+    while not rospy.is_shutdown():
+      joints_str.header.stamp = rospy.Time.now()
+      pub.publish(joints_str)
+      rospy.loginfo("position updated")
+      rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        listener()
+    except rospy.ROSInterruptException:
+        pass
